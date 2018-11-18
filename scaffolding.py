b151f7699ce8376e546a5f172eade189efced055
Ednalyn C. De Dios <ednalyn.dedios@gmail.com>
2018-11-18 00:31:41 -0600

    Create scaffolding.py

---
+++ scaffolding.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+""" Scaffolding: Starter template for code written in Python. """
+
+import os
+import sys
+
+
+def clear():
+	os.system("cls" if os.name == "nt" else "clear")
+
+
+def main():
+    """Main entry point for the script."""
+    pass
+
+
+if __name__ == '__main__':
+    sys.exit(main())
+
+
+
+
+
+
+
+
+
+
+__author__ = "Ednalyn C. De Dios, et al."
+__copyright__ = "Copyright 2018, X Project"
+__credits__ = ["One Fish", "Two Fish", "Blue Fish", "Red Fish"]
+__license__ = "MIT"
+__version__ = "1.0.0"
+__maintainer__ = "Ednalyn C. De Dios"
+__email__ = "ednalyn.dedios@gmail.com"
+__status__ = "Prototype"
